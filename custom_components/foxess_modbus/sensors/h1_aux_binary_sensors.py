5e9d48214461dc6c91af2a32b9edbd65455949c6
Nathan Marlor <nathan_marlor@hotmail.com>
2023-03-09 14:43:01 +0000

    Ensured binary sensors are HA compliant

--- custom_components/foxess_modbus/sensors/h1_aux_binary_sensors.py
+++ custom_components/foxess_modbus/sensors/h1_aux_binary_sensors.py
@@ -12,13 +12,13 @@ SENSORS: dict[str, SensorDescription] = {
         key="time_period_1_enabled",
         address=41001,
         name="Period 1 - Enabled",
-        post_process=lambda v: bool(v),
+        post_process=lambda v: "On" if v else "Off",
     ),
     "time_period_2_enabled": SensorDescription(
         key="time_period_2_enabled",
         address=41004,
         name="Period 2 - Enabled",
-        post_process=lambda v: bool(v),
+        post_process=lambda v: "On" if v else "Off",
     ),
 }
 
