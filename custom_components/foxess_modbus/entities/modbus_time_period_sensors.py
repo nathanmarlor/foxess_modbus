88798983fad37f004b9f834d8de28f4bd129acf5
Antony Male <antony.male@gmail.com>
2023-03-18 19:07:04 +0000

    Fix lints

--- custom_components/foxess_modbus/entities/modbus_time_period_sensors.py
+++ custom_components/foxess_modbus/entities/modbus_time_period_sensors.py
@@ -6,7 +6,6 @@ from datetime import time
 from homeassistant.components.binary_sensor import BinarySensorDeviceClass
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.components.binary_sensor import BinarySensorEntityDescription
-from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.components.sensor import SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
