f8710f1db03a559eb7c8e820075929c4481c60c2
Antony Male <antony.male@gmail.com>
2023-03-28 14:02:52 +0100

    Tidy up the validation

--- custom_components/foxess_modbus/services/update_charge_period_service.py
+++ custom_components/foxess_modbus/services/update_charge_period_service.py
@@ -17,20 +17,37 @@ from ..modbus_controller import ModbusController
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
-def _start_end_must_be_present_if_enabled(data):
+def _seconds_must_be_zero(value: time) -> time:
+    if value.second != 0:
+        raise vol.Invalid("Seconds component must be 0 if specified")
+    return value
+
+
+def _start_end_must_be_present_if_enabled(data: dict[str, Any]) -> dict[str, Any]:
     if data["enable_force_charge"]:
         if not "start" in data:
             raise vol.Invalid(
-                "'start' must be specified if 'enable_force_charge' is True"
+                "'start' must be specified if 'enable_force_charge' is True",
+                path=["start"],
             )
         if not "end" in data:
             raise vol.Invalid(
-                "'end' must be specified if 'enable_force_charge' is True"
+                "'end' must be specified if 'enable_force_charge' is True", path=["end"]
             )
+    return data
 
 
-def _end_must_be_after_start(data):
-    pass
+def _end_must_be_after_start(data: dict[str, Any]) -> dict[str, Any]:
+    if "start" in data and "end" in data:
+        start = data["start"]
+        end = data["end"]
+        if end.hour < start.hour or (
+            end.hour == start.hour and end.minute <= start.minute
+        ):
+            raise vol.Invalid(
+                "'end' must be at least 1 minute after 'start'", path=["end"]
+            )
+    return data
 
 
 _SCHEMA = vol.Schema(
@@ -46,9 +63,11 @@ _SCHEMA = vol.Schema(
             vol.Required(
                 "enable_charge_from_grid", description="Enable charge from grid"
             ): cv.boolean,
-            vol.Optional("start", description="Period Start"): cv.time,
+            vol.Optional("start", description="Period Start"): vol.All(
+                cv.time, _seconds_must_be_zero
+            ),
             vol.Optional("end", description="Period End"): vol.All(
-                cv.time, vol.Range(min=time(hour=0, minute=1))
+                cv.time, vol.Range(min=time(hour=0, minute=1)), _seconds_must_be_zero
             ),
         },
         _start_end_must_be_present_if_enabled,
