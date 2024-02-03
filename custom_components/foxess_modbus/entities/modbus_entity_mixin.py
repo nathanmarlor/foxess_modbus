b15dc5654a446c5b1ee1ca73a3ed674a4c695f1a
Antony Male <antony.male@gmail.com>
2024-02-03 18:16:24 +0000

    Fix mypy errors

--- custom_components/foxess_modbus/entities/modbus_entity_mixin.py
+++ custom_components/foxess_modbus/entities/modbus_entity_mixin.py
@@ -113,7 +113,7 @@ class ModbusEntityMixin(
         else:
             attr_name = "FoxESS - Modbus"
 
-        return DeviceInfo(  # type: ignore
+        return DeviceInfo(
             # services/utils.py relies on the order of entries here. Update that if you update this!
             identifiers={(DOMAIN, inv_model, conn_type, friendly_name)},  # type: ignore
             name=attr_name,
