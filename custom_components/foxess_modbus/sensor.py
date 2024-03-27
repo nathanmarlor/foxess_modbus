38982717821dd8568288e15d051d49e49d68b444
Antony Male <antony.male@gmail.com>
2024-03-27 21:12:56 +0000

    Fix other things which depended on the structure of hass.data
    Fixes: #576

--- custom_components/foxess_modbus/sensor.py
+++ custom_components/foxess_modbus/sensor.py
@@ -7,7 +7,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import CONTROLLERS
+from .common.types import HassData
 from .const import DOMAIN
 from .entities.connection_status_sensor import ConnectionStatusSensor
 from .inverter_profiles import create_entities
@@ -18,7 +18,8 @@ _LOGGER = logging.getLogger(__package__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_devices: AddEntitiesCallback) -> None:
     """Setup sensor platform."""
 
-    controllers = hass.data[DOMAIN][entry.entry_id][CONTROLLERS]
+    hass_data: HassData = hass.data[DOMAIN]
+    controllers = hass_data[entry.entry_id]["controllers"]
 
     for controller in controllers:
         async_add_devices([ConnectionStatusSensor(controller)])
