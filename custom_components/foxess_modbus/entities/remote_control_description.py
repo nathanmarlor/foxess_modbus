692e39c6536e44a937d92c45341effdd05d77af6
Antony Male <antony.male@gmail.com>
2024-04-06 20:08:09 +0100

    Map pwr_limit_bat_up

--- custom_components/foxess_modbus/entities/remote_control_description.py
+++ custom_components/foxess_modbus/entities/remote_control_description.py
@@ -46,7 +46,7 @@ REMOTE_CONTROL_DESCRIPTION = ModbusRemoteControlFactory(
                 max_soc=41010,
                 invbatpower=31022,
                 battery_soc=31024,
-                pwr_limit_bat_up=None,
+                pwr_limit_bat_up=44012,
                 pv_voltages=[39070, 39072],
             ),
             models=Inv.H1_G2,
