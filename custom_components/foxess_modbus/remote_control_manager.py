f708bf47301940f0a99f6cc8e4869629cd1b6a29
Dave Foster <b5djf@yahoo.com>
2024-09-15 09:09:21 +0100

    Update remote_control_manager.py

--- custom_components/foxess_modbus/remote_control_manager.py
+++ custom_components/foxess_modbus/remote_control_manager.py
@@ -272,10 +272,10 @@ class RemoteControlManager(EntityRemoteControlManager, ModbusControllerEntity):
     async def _enable_remote_control(self, fallback_work_mode: WorkMode) -> None:
         # We set a fallback work mode so that the inverter still does "roughly" the right thing if we disconnect
         # (This might not be available, e.g. on H1 LAN)
-        if fallback_work_mode is not None and self._addresses.work_mode_map is not None:
+        if fallback_work_mode is not None and self._addresses.work_mode is not None and self._addresses.work_mode_map is not None:
             fallback_work_mode_value = self._addresses.work_mode_map[fallback_work_mode]
             current_work_mode = self._read(self._addresses.work_mode, signed=False)
-            if current_work_mode != fallback_work_mode_value and self._addresses.work_mode is not None:
+            if current_work_mode != fallback_work_mode_value:
                 await self._controller.write_register(self._addresses.work_mode, fallback_work_mode_value)
 
         if not self._remote_control_enabled:
