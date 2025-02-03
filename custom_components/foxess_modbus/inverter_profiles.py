e0d12e073b494bc56fbc789223b4a4daee907164
Antony Male <antony.male@gmail.com>
2025-02-03 09:01:35 +0000

    Update KH invalid registers
    The KH_PRE133 reads register 31054, but this is in one of the invalid
    ranges. Extend the range upwards a bit to accommodate: this shouldn't
    affect any other inverter versions, as the range stops us bridging up to
    32000 anyway, so we won't read registers 31045-31054 unless we need to.
    
    See: #762

--- custom_components/foxess_modbus/inverter_profiles.py
+++ custom_components/foxess_modbus/inverter_profiles.py
@@ -84,7 +84,7 @@ H3_REGISTERS = SpecialRegisterConfig(
 )
 # See https://github.com/nathanmarlor/foxess_modbus/pull/512
 KH_REGISTERS = SpecialRegisterConfig(
-    invalid_register_ranges=[(41001, 41006), (41012, 41012), (41019, 43999), (31045, 31999)],
+    invalid_register_ranges=[(41001, 41006), (41012, 41012), (41019, 43999), (31055, 31999)],
     individual_read_register_ranges=[(41000, 41999)],
 )
 # See https://github.com/nathanmarlor/foxess_modbus/discussions/553
